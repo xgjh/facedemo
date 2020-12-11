e0bfd98d55eba97397b09bc07561bebf3acf8148
谷瑞琴 <15730299469@163.com>
2020-12-11 14:32:56 +0800

    测试

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,22 @@
 ﻿
 const webpack = require('webpack');
 module.exports = {
+    devServer: {
+        port: 8080,
+        proxy: {
+            '/api': {
+                // target: 'http://180.76.172.91:8008/',
+                target: 'http://192.168.170.68:9000/', 
+                // target host
+                ws: true,
+                disableHostCheck: true,
+                changeOrigin: true,  // needed for virtual hosted sites
+                pathRewrite: {
+                    '^/api': ''    //代理的路径11
+                }
+            }
+        }
+    },
     publicPath: './',
     outputDir: 'docs'
 }
